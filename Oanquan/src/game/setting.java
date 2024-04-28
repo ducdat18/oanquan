51b86396bdd839282c9c9469a2c813aa82958b62
ducdat18 <153488971+ducdat18@users.noreply.github.com>
2024-04-29 04:59:42 +0700

    New

---
+++ Oanquan/src/game/setting.java
@@ -0,0 +1,25 @@
+package game;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class setting extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load FXML file
+        Parent root = FXMLLoader.load(getClass().getResource("setting.fxml"));
+
+        // Set up the primary stage
+        primaryStage.setTitle("Testing UI");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
